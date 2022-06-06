b9d22c0db8ad15e4c9acd9b6d53f45fed8fb39ce
Samly Panton <smov.org@gmail.com>
2022-06-06 01:07:50 +0000

    LandingPage Update

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,4 +1,4 @@
-//import Hero from "../assets/images/algo.png";
+import Hero from "../assets/images/algo.png";
 import { Link } from "react-router-dom";
 
 export default function LandingPage(){
@@ -6,7 +6,7 @@ export default function LandingPage(){
     <div className="container col-xxl-8 px-4 py-5">
     <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
       <div className="col-10 col-sm-8 col-lg-6">
-        {/* <img src={Hero} className="d-block mx-lg-auto img-fluid hero_img" alt="trust logo" width="800" height="800" loading="lazy" /> */}
+        <img src={Hero} className="d-block mx-lg-auto img-fluid hero_img" alt="trust logo" width="800" height="800" loading="lazy" />
       </div>
       <div className="col-lg-6">
         <h1 className="display-5 fw-bold lh-1 mb-3">Diatomix!</h1>
