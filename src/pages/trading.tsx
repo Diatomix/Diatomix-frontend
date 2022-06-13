4dc3ca32bc18840a02faed7c7d8429e33d4e5a46
Samly Panton <smov.org@gmail.com>
2022-06-13 20:22:25 +0000

    Update

--- src/pages/trading.tsx
+++ src/pages/trading.tsx
@@ -1,8 +1,8 @@
 //import Header from '../components/Header';
 //import BoundedLayout from '../components/GridLayout';
 import { useContext } from 'react';
-import LandingPage from '../components/LandingPage';
-import NavBar from '../components/NavBar';
+// import LandingPage from '../components/LandingPage';
+// import NavBar from '../components/NavBar';
 import { AppContext } from '../contexts/app-context';
 import GridLayout from '../components/GridLayout';
 import Header from '../components/Header';
